fa1793f1e6d56523c634d2b946eeb3331c7ac68c
Federico Mariani <fmariani1@ibm.com>
2026-09-07 15:08:05 +0200

    Update to 4.18.4 ER2/CS2 dev build versions

--- CamelJBang.java
+++ CamelJBang.java
@@ -19,10 +19,10 @@
 
 //JAVA 17+
 //REPOS central=https://repo1.maven.org/maven2,redhat.ga=https://maven.repository.redhat.com/ga/
-//JAVA_OPTIONS -Dcamel.jbang.camelSpringBootVersion=4.18.3.redhat-00006 -Dcamel.jbang.quarkusGroupId=com.redhat.quarkus.platform -Dcamel.jbang.quarkusArtifactId=quarkus-bom -Dcamel.jbang.quarkusVersion=3.33.3.redhat-00001
-//DEPS org.apache.camel:camel-bom:${camel.jbang.version:4.18.3.redhat-00005}@pom
-//DEPS org.apache.camel:camel-jbang-core:${camel.jbang.version:4.18.3.redhat-00005}
-//DEPS org.apache.camel.kamelets:camel-kamelets:${camel-kamelets.version:4.18.3.redhat-00004}
+//JAVA_OPTIONS -Dcamel.jbang.camelSpringBootVersion=4.18.4.redhat-00001 -Dcamel.jbang.quarkusGroupId=com.redhat.quarkus.platform -Dcamel.jbang.quarkusArtifactId=quarkus-bom -Dcamel.jbang.quarkusVersion=3.33.3.redhat-00001
+//DEPS org.apache.camel:camel-bom:${camel.jbang.version:4.18.4.redhat-00001}@pom
+//DEPS org.apache.camel:camel-jbang-core:${camel.jbang.version:4.18.4.redhat-00001}
+//DEPS org.apache.camel.kamelets:camel-kamelets:${camel-kamelets.version:4.18.4.redhat-00001}
 
 package main;
 
