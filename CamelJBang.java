26a183d769a413fee4fb1b769f73560ea3d31324
Gaelle Fournier <gaelle.fournier.work@gmail.com>
2026-09-08 18:11:20 +0200

    Add config for extra repos for CEQ

--- CamelJBang.java
+++ CamelJBang.java
@@ -19,6 +19,7 @@
 
 //JAVA 17+
 //REPOS central=https://repo1.maven.org/maven2,redhat.ga=https://maven.repository.redhat.com/ga/
+//JAVA_OPTIONS -Dcamel.extra.repos=redhat.ga=https://maven.repository.redhat.com/ga/
 //DEPS org.apache.camel:camel-bom:${camel.jbang.version:4.22.0}@pom
 //DEPS org.apache.camel:camel-jbang-core:${camel.jbang.version:4.22.0}
 //DEPS org.apache.camel.kamelets:camel-kamelets:${camel-kamelets.version:4.22.0}
